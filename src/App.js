cc77685a9831a7012f5da4adc3f04bc6bf3e2b3f
IsmailS <ismailsidhpuri@gmail.com>
2021-05-16 09:59:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Page Collate</h1>
+      <h2>
+        Purpose of this is to collate pages from multiple documents in different
+        count.
+      </h2>
+      <p>
+        This is for reading of daily marsiya and munajaat bund by bund. So
+        everyday I reciet lets say 2 band of ya sayyeda, 2 band of fulkul
+        husain, 1 band of zikrul husain, 3 band of 1441 munajaat, 3 band of and
+        for munajaat I want to receit 1st and last band everyday.
+      </p>
+    </div>
+  );
+}
